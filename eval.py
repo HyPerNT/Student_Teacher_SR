fd961f3c2553cdb4ad595c457f89351de937f600
HyPerNT <brentoncandelaria@gmail.com>
2022-10-25 01:13:20 -0600

    Added documentation to existing code, will make more useful changes later

--- eval.py
+++ eval.py
@@ -1,46 +1,85 @@
+"""RPN Calculator for SR implementations
+
+Author: Brenton Candelaria
+
+The aim of this file is to provide functionality in interpreting strings of text representing math functions in RPN and provide numerical
+evaluations as an output. This file provides two functions that both work over NumPy arrays: Eval(), and EvalPt(). Both expect a string
+to be evaluated, but also an array to evaluate on. EvalPt() expects a vector to parse and evaluate, and Eval() works over an array of such
+vectors, deferring to EvalPt() for evaluation.
+
+In implementation, complex results/those that occur outside of a function's domain (such as lg(0)) are returned as np.nan from EvalPt()
+This behavior can be changed in the future, but suffices to tell the user that the computation could not be computed gracefully.
+"""
+
 import math
 import numpy as np
-from language import *
-import traceback
+from language import * # Utilize the basic context-free grammar for math
+import traceback # For some light error-logging, in the worst case
 
 
 def EvalPt(expr: str, x: np.ndarray) -> np.ndarray:
-    stack = []
-    orig_expr = expr
-    i = 0
-    x1 = 0
-    x2 = 0
-    k = i
+    """Evaluates a unit vector x using the expression str
+
+        Parameters
+        ----------
+            expr : A (syntactically correct) expression to be parsed in RPN, like "X{0}X{1}+"
+            x : A numpy array that corresponds to a vector, each component being a scalar variable in the function
+
+        Returns
+        -------
+            np.ndarray : An output vector of length len(x)+1, appending the result to the end
+                May append np.nan on failed computations
+    """
+    stack = [] # Tracks numbers on the stack
+    orig_expr = expr # Track original expr, since we overwrite it during parsing
+    x1 = 0 # First arg, if any
+    x2 = 0 # Second arg, if any
+    i = 0 # Tracks position in overall expression, after edits (dropping braces during parsing)
+    k = i # Tracks position in original expression, sans edits
+
+    # Surround in try-catch, we might be handed invalid syntax and cannot handle it otherwise
     try:
+        # Parse to end of the expr
         while i < len(expr):
-            symbol = expr[i]
+            symbol = expr[i] # Get next symbol to process
             i += 1
-            k += 1
-            if symbol not in non_literals:  # evaluate
-                if symbol in constants:
-                    if symbol.isalpha():
+            k += 1 
+            # Inc i and k since we're now processing it
+
+            if symbol not in non_literals:  # Evaluate vars, constants
+                if symbol in constants: # Eval constants (e, pi, 0, 1)
+                    if symbol.isalpha(): # On letter constants, append their value to the stack
                         if symbol == "p":
                             stack.append(math.pi)
                         else:
                             stack.append(math.e)
-                    else:
+                    else: # Append the constant (0 or 1) to the stack
                         stack.append(int(symbol))
-                else:
+                else: # Encountered a variable, process it
                     first = expr.index("{")
                     last = expr.index("}")
-                    stack.append(x.item(int(expr[first+1:last])))
+                    stack.append(x.item(int(expr[first+1:last]))) # Extract the indexed var in the expression, add to stack
                     expr = expr.replace('{', '', 1)
-                    expr = expr.replace('}', '', 1)
+                    expr = expr.replace('}', '', 1) # Replace braces in expr so next instance of braces can be correctly found
                     i += last-first-1
                     k = i+2
-            else:
+                    # Update i to reflect new position in expr, k to reflect updated position in orig_expr
+            
+            else: # On unary or binary operators
                 if symbol in binary:
+                    # Get args
                     x1 = stack.pop()
                     x2 = stack.pop()
+
+                    # Pre-check type of args in case sh*t hit the fan and we haven't caught it yet
                     if (type(x1) is complex) or (type(x2) is complex):
                         return np.append(x, np.nan)
+
+                    # Conv to float and work with it
                     x1 = float(x1)
                     x2 = float(x2)
+
+                    # "Good code documents itself"
                     if symbol == "+":
                         stack.append(x1 + x2)
                     elif symbol == "-":
@@ -48,18 +87,26 @@ def EvalPt(expr: str, x: np.ndarray) -> np.ndarray:
                     elif symbol == "*":
                         stack.append(x1 * x2)
                     elif symbol == "/":
+                        # Check for div by 0
                         if (x2 == 0):
                             return np.append(x, np.nan)
                         stack.append(x1 / x2)
                     elif symbol == "^":
+                        # Prevent the CPU from cooking itself
                         if (abs(x2) >= 20) or (x1 == 0 and x2 <= 0):
                             return np.append(x, np.nan)
                         stack.append(x1 ** x2)
-                else:
+
+                else: # Unary operators
                     q = stack.pop()
+
+                    # Same pre-check
                     if type(q) is complex:
                         return np.append(x, np.nan)
+
                     q = float(q)
+
+                    # "Good code documents itself"
                     if symbol == "~":
                         stack.append(q * -1)
                     elif symbol == ">":
@@ -70,6 +117,8 @@ def EvalPt(expr: str, x: np.ndarray) -> np.ndarray:
                         stack.append(math.sin(q))
                     elif symbol == "C":
                         stack.append(math.cos(q))
+
+                    # Check domains on arctrig fns, ln
                     elif symbol == "s":
                         if q < -1 or q > 1:
                             return np.append(x, np.nan)
@@ -88,33 +137,45 @@ def EvalPt(expr: str, x: np.ndarray) -> np.ndarray:
                         if q <= 0:
                             return np.append(x, np.nan)
                         stack.append(math.log(q))
+        
+        # Finished parsing the fn! Get the result (might err here if there's nothing to pop)
         q = stack.pop()
         if (type(q) is complex):
             return np.append(x, np.nan)
+
         return np.append(x, float(q))
+
     except Exception:
+        # Lazy error handling, give stack trace and also show where in the fn we went wrong
         traceback.print_exc()
         print(f"Stack: {stack}\nSymbol: {symbol}\n" +
               f"expr={orig_expr}\n{' '*(k+4) + '^'}\
               \nEval:{str(x1) + str(symbol) + str(x2)}\n\n\n")
+
+        # A wrong answer is better than no answer
         return np.append(x, np.nan)
 
 
 def Eval(expr: str, x: np.ndarray) -> np.ndarray:
+    """Evaluate an array of pts on the same expression, returning the full array with the corresponding results.
+
+        Parameters
+        ----------
+            expr : A (syntactically correct) expression to be parsed in RPN, like "X{0}X{1}+"
+            x : A numpy array that corresponds to a list of vectors, like those accepted by EvalPt()
+
+        Returns
+        -------
+            np.ndarray : An output array with shape (n, m+1) with appended column of results
+    """
     newarr = np.array([])
-    i=0
+    i = 0
+
     for xi in x:
-        i+=1
-        x2 = EvalPt(expr, xi)
-        newarr = np.append(newarr, x2)
-        if np.isnan(x2[len(x2)-1]):
-            s = list(x.shape)
-            s[1] += 1
-            i = len(newarr)/s[1]
-            s[0] = int(i)
-            newarr.shape = s
-            return newarr
+        i += 1
+        x2 = EvalPt(expr, xi) # Get ans from EvalPt
+        newarr = np.append(newarr, x2) # Append
     s = list(x.shape)
     s[1] += 1
-    newarr.shape = s
+    newarr.shape = s # Correct shape to "look like" the old arr
     return newarr
--- rd.py
+++ rd.py
@@ -1,3 +1,11 @@
+"""Rough Draft for Interpretable Student-Teacher NNs on SR
+
+Authors: Brenton Candelaria and Sophia Novo-Gradac
+
+Testing ground for code, has some solid work done in advance to make more tedious work easier in the future
+
+"""
+
 import numpy as np
 import matplotlib as plt
 from eval import Eval, EvalPt
@@ -21,25 +29,55 @@ DEFAULT_TEST_SIZE = 0.2
 # Identity is always identity matrix
 # We can preserve arguments by passing forward args as identity matrix forward to next layer, same with operations we've gathered
 # Lin. combinations look like [ ... a b c.... d] for something like ax_n + bx_n+1 + cx_n+2 + dx_m
-
+# ... I can't figure out how to do multiplication of two variables in an array...
 
 
 class mystery_function():
-    """Dumb ass class
-    """
-    def __init__(self, fn, dim, sample_size=DEFAULT_SAMPLE_SIZE, scale=DEFAULT_SCALE, test_size=DEFAULT_TEST_SIZE, noisy=False, noise_factor=DEFAULT_NOISE):
+    """A class to model a given fn, also handles creation/storage of data to train/test on"""
+    def __init__(self, fn, dim, gen_data=False, sample_size=DEFAULT_SAMPLE_SIZE, scale=DEFAULT_SCALE, test_size=DEFAULT_TEST_SIZE, noisy=False, noise_factor=DEFAULT_NOISE):
+        """Init function, generates a function for fn in dim variables, includes optional params for tuning the data generated
+        
+            Parameters
+            ----------
+                fn : The function, as a string, to be modeled
+                dim : The number of variables in the function, typically just the length of each vector that would be in the input space
+                gen_data : Whether to generate data on init, False by default
+                sample_size : The number of samples to generate in total, configurable at head of this file
+                scale : Scale/range to vary the Gaussian distribution of samples by, [0, 1) by default
+                test_size : Portion of sample_size to be used for test batch, should be a float in [0, 1]
+                noise : Whether to introduce Gaussian noise to the generated dataset, false by default
+                noise_factor : Scale of Gaussian noise added when it is introduced
+                """
         self.fn = fn
         self.dim = dim
-        self.gen_data(fn, sample_size=sample_size, scale=scale, test_size=test_size, noisy=noisy, noise_factor=noise_factor)
+        if gen_data:
+            self.gen_data(fn, sample_size=sample_size, scale=scale, test_size=test_size, noisy=noisy, noise_factor=noise_factor)
 
     def gen_data(self, fn, sample_size=DEFAULT_SAMPLE_SIZE, scale=DEFAULT_SCALE, test_size=DEFAULT_TEST_SIZE, noisy=False, noise_factor=DEFAULT_NOISE):
-        r = np.random.rand(int(sample_size*(1-test_size)), self.dim) * scale
-        self.x_train = r
-        self.y_train = Eval(fn, r)[:,-1]
+        """
+        Method to generate data for our candidate fn, makes a random numpy array and defers to Eval() for answers
 
-        r = np.random.rand(int(sample_size*test_size), self.dim) * scale
+            Parameters
+            ----------
+                fn : The function, as a string, to be modeled
+                dim : The number of variables in the function, typically just the length of each vector that would be in the input space
+                gen_data : Whether to generate data on init, False by default
+                sample_size : The number of samples to generate in total, configurable at head of this file
+                scale : Scale/range to vary the Gaussian distribution of samples by, [0, 1) by default
+                test_size : Portion of sample_size to be used for test batch, should be a float in [0, 1]
+                noise : Whether to introduce Gaussian noise to the generated dataset, false by default
+                noise_factor : Scale of Gaussian noise added when it is introduced
+                """
+
+        r = np.random.rand(int(sample_size*(1-test_size)), self.dim) * scale # Generate input train array according to params
+        self.x_train = r
+        self.y_train = Eval(fn, r)[:,-1] # Init x_train and y_train
+
+        r = np.random.rand(int(sample_size*test_size), self.dim) * scale # Generate seperate test array according to params
         self.x_test = r
-        self.y_test = Eval(fn, r)[:,-1]
+        self.y_test = Eval(fn, r)[:,-1]  # Also init x_test and y_test
+        
+        # The below just adds noise as specified, centered at 0
         if noisy:
             self.x_train = self.x_train + noise_factor * np.random.rand(int(sample_size * (1 - test_size)), self.dim) - 0.5      
             for i in self.y_train:
@@ -51,54 +89,122 @@ class mystery_function():
 
 
 def getNN(layers, nodes, in_shape, rate=0.01, name='nn'):
+    """Builds a simple FF-NN with the n layers and m nodes per layer, as specified.
+        Each layer is really a densely connected layer followed by a dropout to prevent overfitting, but details
+
+        Parameters
+        ----------
+            layers : Number of layers the network should have
+            nodes : Number of nodes per layer
+            in_shape : Input shape of the NN
+            rate : Dropout rate, 0.01 by default
+            name : Name for the network, useful for tracking many NNs
+    """
+    # Make template NN
     model = tf.keras.Sequential([tf.keras.Input(shape=in_shape)],name=name)
+
+    # Iteratively add layers
     for _ in range(layers):
         model.add(tf.keras.layers.Dense(nodes, activation='relu'))
         model.add(tf.keras.layers.Dropout(rate=rate))
+
+    # Add final layer for regression
     model.add(tf.keras.layers.Dense(1))
     return model
 
 def iterateNN(in_shape, fn, layer_range=10, node_range=512, by=2, method="*", rate=0.01, name='nn'):
+    """Generates many NNs as candidates for training, useful to find "an ideal" architecture before we craft a bad one by hand
+
+        Parameters
+        ----------
+            in_shape : Shape of the input layer
+            fn : The expression to use for training (Constant across all NNs)
+            layer_range : Number of layers to vary over, incrementally
+            node_range : Number of nodes to vary over, incrementally or multiplicatively
+            by : Factor to use when adding more nodes, either additive or multiplicative
+            method : Whether to add more nodes "+", or to multiply # of nodes "*"
+            rate : Dropout rate
+            name : Name for each NN, used as a template since we append details about architecture to the name
+
+        Returns
+        -------
+            A tuple, of the following form:
+                (x, y, z)
+            
+            x : A list of the best tracked architectures (might reveal patterns)
+            y : The best model produced
+            z : A full history of all model performance
+
+            Each entry of x is a list like:
+                [  [i, j]  ,   loss  ]
+
+            Where:
+                i : Number of layers
+                j : Number of nodes / layer
+                loss : Loss of the model
+
+            Each entry of z is a list like:
+                [   [i, j]   ,   loss   ,   params   ]
+
+            Where:
+                i : Number of layers
+                j : Number of nodes / layer
+                loss : Loss of the model
+                params : Number of configurable params of that model
+
+    """
+
     best_nn_loss = -1
     best_ij = [-1,-1]
     best_confs = []
     name_base = name
     best_model = None
     history = []
+    # Init all vars for tracking to bunk data for now
+
+    # Vary across layers first
     for i in range(layer_range):
         j = 1
+        # Vary across # of nodes per layer
         while j <= node_range:
-            name = name_base + f"_{i}-layers_{j}-nodes"
+            name = name_base + f"_{i}-layers_{j}-nodes" # Generate name for this model, describe it so we can see something useful in terminal
             print(f'\n\nTraining {name}...\n{(len(name)+12)*"-"}')
-            model = getNN(i, j, in_shape, rate, name)
-            model.compile(optimizer=tf.keras.optimizers.Adam(), loss='mean_squared_error')
-            train = model.fit(fn.x_train, fn.y_train, epochs=EPOCHS)
-            predict = model.evaluate(fn.x_test, fn.y_test)
+            model = getNN(i, j, in_shape, rate, name) # Build a NN of that architectyre
+            model.compile(optimizer=tf.keras.optimizers.Adam(), loss='mean_squared_error') # Compile it
+            train = model.fit(fn.x_train, fn.y_train, epochs=EPOCHS) # Train
             train = min(train.history['loss'])
-            model.summary()
-            history.append([[i,j], train+predict, model.count_params()])
-            if train+predict < best_nn_loss or best_nn_loss == -1:
-                best_nn_loss = train+predict
+            predict = model.evaluate(fn.x_test, fn.y_test) # Test
+
+            model.summary() # Print details, more terminal output
+
+            history.append([[i,j], train+predict, model.count_params()]) # Append this model to the full history
+
+            if train + predict < best_nn_loss or best_nn_loss == -1: # This model is the new best
+                best_nn_loss = train + predict
                 best_ij = [i,j]
-                best_confs.append([best_ij, best_nn_loss])
+                best_confs.append([best_ij, best_nn_loss]) # Append to best models
                 best_model = model
-                print(f"{(len(best_ij)+16)*'-'}\nNew Best: {best_ij}\nScore: {best_nn_loss:1.4f}\n{(len(best_ij)+16)*'-'}")
+                print(f"{(len(best_ij)+16)*'-'}\nNew Best: {best_ij}\nScore: {best_nn_loss:1.4f}\n{(len(best_ij)+16)*'-'}") # Log to term
+
+            # Increase # of nodes as specified
             if method=="*":
-                j*=by
+                j *= by
             elif method=="+":
-                j+=by
+                j += by
+
+    # Print best results, and return
     print(f"Best Configuration: {best_ij[0]} layers and {best_ij[1]} nodes/layer, loss of {best_nn_loss:1.4f}")
     return best_confs, best_model, history
 
 def main():
     start = timeit.default_timer()
-    fn1 = mystery_function("0>>x{0}0>>x{1}^*/", 2, sample_size=10_000, scale=10, noisy=True)
+    fn1 = mystery_function("0>>x{0}0>>x{1}^*/", 2, sample_size=10_000, scale=10, gen_data=True, noisy=True) # This is just 1/2 mv^2, basically
     # sin = mystery_function("x{0}S", 1, noisy=True)
-
-    # All records like [  [i,j], loss,  ] or [  [i,j], loss, params  ] 
-
-    records, best_nn, all_models= iterateNN((2,), fn1, layer_range=10, name='KE_nn')
+    records, best_nn, all_models= iterateNN((2,), fn1, layer_range=10, name='KE_nn') # Store results from iteration
     stop = timeit.default_timer()
+
+    # Print everything out
+    # TODO: Process all of the f*cking data to hopefully get some insight
     print(f"Best models: \n{('-'*32)}\n{records}\n\n")
     print(f"All models: \n{('-'*32)}\n{all_models}\n\n")
     minutes, sec = divmod(stop-start, 60.0)
